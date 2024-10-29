c071da08b73e034da21ba40ff88f6986d49ec239
Juan Mireles <juancnmireles@gmail.com>
2024-10-28 21:18:20 -0400

    Add player model and animate him depending on which direction he is moving

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,33 @@
 #include "raylib.h"
+#include "rlgl.h"
+
+typedef enum { IDLE, WALK_LEFT, WALK_RIGHT, WALK_UP, WALK_DOWN } PlayerState;
 
 int main() {
-    const int screenWidth{1280};
-    const int screenHeight{800};
+    const int SCREEN_WIDTH{1280};
+    const int SCREEN_HEIGHT{800};
+    const int PLAYER_SPRITE_COLUMNS{6};
+    const int PLAYER_SPRITE_ROWS{10};
+    const float FRAME_TIME{0.1f};
 
     // Tell the window to use vsync and work on high DPI displays
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-    InitWindow(screenWidth, screenHeight, "Prince");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Prince");
 
-    Mesh terrainMesh = GenMeshPlane(50.0f, 50.0f, 10, 10);
-    Model terrainModel = LoadModelFromMesh(terrainMesh);
-    Texture2D terrainTexture = LoadTexture("resources/ground_terrain.png");
-    terrainModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = terrainTexture;
+    Texture2D characterSheet = LoadTexture("resources/Player/Player.png");
 
-    Vector3 terrainPosition = {0.0f, 0.0f, 0.0f};
+    int frameWidth = characterSheet.width / PLAYER_SPRITE_COLUMNS;
+    int frameHeight = characterSheet.height / PLAYER_SPRITE_ROWS;
 
-    Vector3 cubePosition = {0.0f, 1.0f, 0.0f};
+    PlayerState playerState{IDLE};
+    Vector3 playerPosition = {0.0f, 1.0f, 0.0f};
+
+    int currentFrame{0};
+    float frameCounter{0.0f};
+    Rectangle frameRectangle = {0.0f, 0.0f, (float)frameWidth,
+                                (float)frameHeight};
+
     Vector3 cameraOffset = {0.0f, 25.0f, 30.0f};
     float moveSpeed = 0.2f;
     float zoomSpeed = 1.0f;
@@ -24,7 +35,7 @@ int main() {
     float maximumZoom = 30.0f;
 
     Camera3D camera = {0};
-    camera.target = cubePosition;
+    camera.target = playerPosition;
     camera.up = (Vector3){0.0f, 1.0f, 0.0f};
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
@@ -34,14 +45,35 @@ int main() {
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_W))
-            cubePosition.z -= moveSpeed;
-        if (IsKeyDown(KEY_S))
-            cubePosition.z += moveSpeed;
-        if (IsKeyDown(KEY_A))
-            cubePosition.x -= moveSpeed;
-        if (IsKeyDown(KEY_D))
-            cubePosition.x += moveSpeed;
+        Vector3 playerMovement = {0.0f, 0.0f, 0.0f};
+        bool isMoving = false;
+
+        if (IsKeyDown(KEY_W)) {
+            playerMovement.z -= moveSpeed;
+            playerState = WALK_UP;
+            isMoving = true;
+        }
+        if (IsKeyDown(KEY_S)) {
+            playerMovement.z += moveSpeed;
+            playerState = WALK_DOWN;
+            isMoving = true;
+        }
+        if (IsKeyDown(KEY_A)) {
+            playerMovement.x -= moveSpeed;
+            playerState = WALK_LEFT;
+            isMoving = true;
+        }
+        if (IsKeyDown(KEY_D)) {
+            playerMovement.x += moveSpeed;
+            playerState = WALK_RIGHT;
+            isMoving = true;
+        }
+
+        if (!isMoving)
+            playerState = IDLE;
+
+        playerPosition.x += playerMovement.x;
+        playerPosition.z += playerMovement.z;
 
         float wheelMove = GetMouseWheelMove();
         cameraOffset.z -= wheelMove * zoomSpeed;
@@ -56,27 +88,59 @@ int main() {
         cameraOffset.y = cameraOffset.z * yToZRatio;
 
         camera.position =
-            (Vector3){cubePosition.x + cameraOffset.x, cameraOffset.y,
-                      cubePosition.z + cameraOffset.z};
-        camera.target = cubePosition;
+            (Vector3){playerPosition.x + cameraOffset.x, cameraOffset.y,
+                      playerPosition.z + cameraOffset.z};
+        camera.target = playerPosition;
+
+        // Frame animation logic
+        frameCounter += GetFrameTime();
+        if (frameCounter >= FRAME_TIME) {
+            frameCounter = 0.0f;
+            currentFrame++;
+
+            // Loop back to the first frame
+            if (currentFrame >= PLAYER_SPRITE_COLUMNS)
+                currentFrame = 0;
+        }
+
+        switch (playerState) {
+            case IDLE:
+                frameRectangle.y = 0.0f; // 1st row
+                break;
+            case WALK_DOWN:
+                frameRectangle.y = (float)frameHeight * 3; // 4th row
+                break;
+            case WALK_UP:
+                frameRectangle.y = (float)frameHeight * 5; // 6th row
+                break;
+            case WALK_LEFT:
+                frameRectangle.y = (float)frameHeight * 4; // 5th row
+                frameRectangle.width = -(float)frameWidth;
+                break;
+            case WALK_RIGHT:
+                frameRectangle.y = (float)frameHeight * 4; // 5th row
+                frameRectangle.width = (float)frameWidth;
+                break;
+        }
+
+        // Update the frame to make it animated
+        frameRectangle.x = (float)(currentFrame * frameWidth);
 
         BeginDrawing();
         ClearBackground(DARKGRAY);
 
         BeginMode3D(camera);
-        DrawModel(terrainModel, terrainPosition, 1.0f, WHITE);
-        DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-        DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+        DrawBillboardRec(camera, characterSheet, frameRectangle, playerPosition,
+                         (Vector2){2.0f, 2.0f}, WHITE);
         EndMode3D();
 
-        DrawText("Cool 3D cube, jk :P", 10, 40, 20, RAYWHITE);
         DrawFPS(10, 10);
 
         EndDrawing();
     }
 
-    UnloadTexture(terrainTexture);
-    UnloadModel(terrainModel);
+    UnloadTexture(characterSheet);
     CloseWindow();
     return 0;
 }
